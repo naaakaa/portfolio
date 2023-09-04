2526301381c5e20e70c081a35796538e308e1dc7
Anastasia <ana.kuk@gmx.net>
2023-09-04 21:38:41 +0200

    Update logo text

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar({ router }) {
     <nav className="navbar">
       <div className="container">
         <Link href="/" className="logo">
-          Lutfhi.
+          AnaKuk.
         </Link>
 
         <ul className="nav-links">
