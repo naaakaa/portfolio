6c2f37479924ea39ed6bf0e3c5a0d6ef4c294f4b
Anastasia <ana.kuk@gmx.net>
2023-09-04 21:00:51 +0200

    Update of three paragraphs

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,15 +15,12 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hallo, I am Anastasia!
             </h1>
-            <p className="description">
-              Saya seorang pekerja lepas pengembang web, fotograper, desainer
-              antarmuka pengguna, dan penulis.
-            </p>
+            <p className="description">More Info coming soon</p>
 
             <Link href="/contact" className="cta">
-              Kontak Saya
+              Contact
             </Link>
           </div>
 
